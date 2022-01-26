69127a5dd15eac4ad5fc7d6cfe7eee9db20d5ebf
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-01-26 17:51:41 -0500

    Done with Requirements

--- fetch-api/src/App.js
+++ fetch-api/src/App.js
@@ -12,55 +12,47 @@ function App() {
     email: "",
     password: ""
   })
-
   const [submitted, setSubmitted] = useState(false)
   const [valid, setValid] = useState(false)
   const [occupation, setOccupation] = useState([])
   const [state, setState] = useState([])
   
-  
+  const {name, email, password} = input
 
   useEffect(() => {
     const getData = async () => {
       let response = await axios.get(URL)
       setOccupation(response.data.occupations)
       setState(response.data.states)
-      // console.log(response.data.states)
       }
       getData()
   }, [])
  
-
-  const handleINameChange = (event) => {
-    setInput({...input, name: event.target.value})
-  }
-  const handleIEmailChange = (event) => {
-    setInput({...input, email: event.target.value})
-  }
-  const handlePasswordChange = (event) => {
-    setInput({...input, password: event.target.value})
-  }
-
- 
-
   const handleSubmit = async(e) => {
     e.preventDefault()
+    if (name && email && password && occupation && state) {
+      setValid(true)
     const response = await axios.post(URL, {
-      name: input.name,
-      email: input.email,
-      password: input.password,
+      name: name,
+      email: email,
+      password: password,
       occupation: occupation,
       state: state
     })
-    if (input.name && input.email && input.password && occupation && state) {
-      setValid(true)
+    console.log(response);
     }
     setSubmitted(true)
-    console.log(response);
+    
 
   }
 
-
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   
 
   return <div className="form-container">
@@ -68,26 +60,26 @@ function App() {
     <form className="register-form" onSubmit={handleSubmit}>
       {submitted && valid ? <div className="succes-message">Success! Thank you for registering</div> : null}
       <input
-        onChange={handleINameChange}
+        onChange={handleChange}
         className='form-field'
-        value={input.name}
+        value={name}
         placeholder='Full Name'
         name="name" />
-      {submitted && !input.name ? <span>Please enter a name</span> : null}
+      {submitted && !name ? <span>Please enter a name</span> : null}
        <input
-        onChange={handleIEmailChange}
+        onChange={handleChange}
         className='form-field'
-        value={input.email}
+        value={email}
         placeholder='Email Address'
-        name="name" />
-      {submitted && !input.email ? <span>Please enter a email address</span> : null}
+        name="email" />
+      {submitted && !email ? <span>Please enter a email address</span> : null}
        <input
-        onChange={handlePasswordChange}
+        onChange={handleChange}
         className='form-field'
-        value={input.password}
+        value={password}
         placeholder='Password'
-        name="name" />
-      {submitted && !input.password ? <span>Please enter a password</span> : null}
+        name="password" />
+      {submitted && !password ? <span>Please enter a password</span> : null}
       <select className="form-field">
         <option disabled={true} value="occupation">-- Select Occupation --</option>
         {
